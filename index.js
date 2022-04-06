4c24fc6c685c4166cdf69393c39452e4289a2478
Marco Berna <mbernaildeons@gmail.com>
2022-04-06 13:12:20 -0500

    Btones ubicados: se ha colocado los botones respectivamente de bajo del buscador

--- index.js
+++ index.js
@@ -2,14 +2,14 @@ function voice(){
 
 
     let rec
-    
+
         if(!("webkitSpeechRecognition"in window)){
             alert("Disculpa mano, no puedes usar la API");
 
         }
         else{
             rec = new webkitSpeechRecognition()
-            rec.lang="es-ES";
+            rec.lang="es-PE";
             rec.continuous=true;
             rec.interim=true;
             rec.addEventListener("result",iniciar)
@@ -18,7 +18,6 @@ function voice(){
     function iniciar(event){
             for (let i = event.resultIndex; i < event.results.length; i++) {
                 document.getElementById("buscador-google").value=event.results[i][0].transcript;
-                
             }
     }
     rec.start();
