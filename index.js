c4fce0b05f125145ef705aaf7ce5df97c39a515c
Marco Berna <mbernaildeons@gmail.com>
2022-04-06 19:58:09 -0500

    FOOTER: Se ha colocado el footer con sus respectivos links y colores

--- index.js
+++ index.js
@@ -2,11 +2,12 @@ function voice(){
 
 
     let rec
-
+        // pregunta si esta activado el microfono
         if(!("webkitSpeechRecognition"in window)){
             alert("Disculpa mano, no puedes usar la API");
 
         }
+        // si esta activado define el idioma y los componentes
         else{
             rec = new webkitSpeechRecognition()
             rec.lang="es-PE";
@@ -15,7 +16,9 @@ function voice(){
             rec.addEventListener("result",iniciar)
 
         }
+        // funcion para que revise el event para que inicie a escribir en el imput
     function iniciar(event){
+        
             for (let i = event.resultIndex; i < event.results.length; i++) {
                 document.getElementById("buscador-google").value=event.results[i][0].transcript;
             }
